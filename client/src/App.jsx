9efdf665734957c5d207d1ffbac57965bf2770ca
unknown <gpimentel@itla.edu.do>
2023-09-09 18:11:44 -0400

    fronted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+//import reactLogo from './assets/react.svg';
+//import viteLogo from './vite.svg'; // Corregí la importación de viteLogo
+//import './App.css';
 
 export default function App() {
+  
+  //lista de elementos a visualizar
 
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState("Gary")
+  const [resultArray, setResultArray] = useState([]);
 
-  let nombre = ""
+  // lee los datos de la api
 
-  const imprimir_nombre = (nombre) => {
-
-    console.log(nombre)
-
+  async function leerDatos() {
+    try {
+      const result = await axios.get("http://localhost:4000/");
+      //console.log(result)
+      setResultArray(result.data);
+    } catch (error) {
+      console.error("Error al cargar datos:", error);
+    }
   }
 
-  function handleChange(event) {
-    this.setState({value: event.target.value});
-  }
+  //
+
+  useEffect(() => {
+    leerDatos();
+  }, []);
+
+
+  // la lista de elementos de la tabla
+
+  const itemList = resultArray.map( (item) => (
+
+    <tr key={item.Id}>
+      <td>{item.nombre}</td>
+      <td>{item.apellido}</td>
+      <td>{item.telefono}</td>
+      <td><Button variant="outline-warning">Borrar</Button>{' '}</td>
+      <td>Editar</td>
+    </tr>
+
+  ));
+
+    //aqui retornamos la pagina a mostrar
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Gary Pimentel</h1>
-      <div className="card">
-        <button onClick={() => {
-          setCount((count) => count + 1)
-          }}>
-          count is {count}
-        </button>
-      </div>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Telefono</th>
+            <th>Borrar</th>
+            <th>Actualizar</th> {/* Cambié la última columna a "Actions" */}
+          </tr>
+        </thead>
+        <tbody>
+          {itemList}
+        </tbody>
+      </Table>
     </>
-  )
+  );
 }
+
 
 
--- server/main.js
+++ server/main.js
@@ -2,10 +2,13 @@
 //const express = require("express")
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 import { adminRouter } from "./src/routes/routes_principal.js";
 
 export const app = express()
+
+app.use(cors())
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(express.json());
